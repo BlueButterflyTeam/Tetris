cda9447bed31eca8f5edefeba16031c3bf225338
Aymeric MASSE <aym.masse@gmail.com>
2016-12-13 18:52:25 -0500

    Added random get from enums

--- TetrominoRotation.h
+++ TetrominoRotation.h
@@ -1,2 +1,6 @@
 #pragma once
 enum TetrominoRotation {NONE, ONCE, TWICE, THRICE, NB_ROTATIONS};
+
+TetrominoRotation getRandomTetrominoRotation() {
+	return static_cast<TetrominoRotation>(rand() % NB_ROTATIONS);
+}
--- TetrominoType.h
+++ TetrominoType.h
@@ -1,2 +1,6 @@
 #pragma once
-enum TetrominoType {SQUARE, I, L, MIRRORED_L, N, MIRRORED_N, T, NUM_TETROMINO_TYPES};
+enum TetrominoType {SQUARE, I, L, MIRRORED_L, N, MIRRORED_N, T, NB_TYPES};
+
+TetrominoType getRandomTetrominoType() {
+	return static_cast<TetrominoType>(rand() % NB_TYPES);
+}
